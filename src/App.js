5b6c4418ca7dcbc8ed4ec8e69a0269ef53739473
Marc Tofan <tofmana@gmail.com>
2024-05-16 20:16:23 -0700

    initial commit, project initialized and product context created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductDetail from "./pages/ProductDetails";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+
 
 const App = () => {
-  return <div className='overflow-hidden'>react app</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/product/:id" element={<ProductDetail />} />
+      </Routes>
+      <Sidebar />
+      <Footer />
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+// Contexts
+import ProductContextProvider from "./contexts/ProductContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ProductContextProvider>
+      <App />
+    </ProductContextProvider>
   </React.StrictMode>
 );
