cef76e52cda028390d79e651b7c6b824732c4ff0
Marc Tofan <tofmana@gmail.com>
2024-05-22 17:32:11 -0700

    useEffect function in Header component improved

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,7 +9,7 @@ import { CartContext } from "../contexts/CartContext";
 
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SidebarContext);
-  const { cart, clearCart, total } = useContext(CartContext);
+  const { cart, clearCart, total, itemAmount } = useContext(CartContext);
 
   return (
     <div
@@ -18,7 +18,7 @@ const Sidebar = () => {
       } w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}
     >
       <div className="flex items-center justify-between py-6 border-b">
-        <div className="uppercase text-sm font-semibold">Shopping bag (0)</div>
+        <div className="uppercase text-sm font-semibold">Shopping bag ({itemAmount})</div>
         <div className="cursor-pointer w-8 h-8 flex justify-center items-center" onClick={handleClose}>
           <IoMdArrowForward className="text-2xl" />
         </div>
